a54222c2f316e42c80ecf1a16164587910ffad67
bezzy0515 <beselaere@hotmail.com>
2024-10-21 21:09:33 -0400

    Step 1.
    Check input variable is a string.

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,7 @@
 """
 Validates strings as palindromes.
 """
+
+def is_palindrome(input_value):
+    if not isinstance(input_value, str):        # Make sure input is a string
+        raise ValueError()
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,20 @@
 """
 Tests the palindrome module
 """
+
+import palindrome
+import pytest
+
+"""
+Validate input variables data type.
+"""
+
+
+def test_input_data_for_string():
+    test_int = 12
+    test_float = 6.8
+
+    with pytest.raises(ValueError):
+        palindrome.is_palindrome(test_int)
+    with pytest.raises(ValueError):
+        palindrome.is_palindrome(test_float)
